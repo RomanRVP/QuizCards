8d26268f17ce651d3111ebf123e5ce206839b35f
RomanRVP <spn02@ukr.net>
2023-02-04 17:46:02 +0200

    the first view has been created, which so far returns only the page for registration and login

--- account/views.py
+++ account/views.py
@@ -1,2 +1,10 @@
+from django import views
 from django.shortcuts import render
 
+
+class RegistrationAndLogin(views.View):
+    """
+    A class that is responsible for creating a new account and logging into it.
+    """
+    def get(self, request, *args, **kwargs):
+        return render(request, 'account/signup_and_signin.html')
