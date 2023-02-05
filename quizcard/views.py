54b478b32242bba68f3ad98dee5eaef228681049
RomanRVP <spn02@ukr.net>
2023-02-05 23:52:09 +0200

    created a simple View for main page

--- quizcard/views.py
+++ quizcard/views.py
@@ -1,2 +1,11 @@
+from django import views
 from django.shortcuts import render
 
+
+class HomePage(views.View):
+    """
+    This is the main page.
+    """
+
+    def get(self, request, *args, **kwargs):
+        return render(request, 'quizcard/main_page.html')
