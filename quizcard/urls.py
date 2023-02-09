0e4220118b00957ea8d0f653c2a21715f183c596
RomanRVP <spn02@ukr.net>
2023-02-09 20:39:13 +0200

    creating a folder for the quizcard API and include its URLs

--- quizcard/urls.py
+++ quizcard/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 
 from .views import HomePage
 
 
 urlpatterns = [
     path('', HomePage.as_view(), name='main_page'),
+    path('api/', include('quizcard.api.urls'))
 ]
