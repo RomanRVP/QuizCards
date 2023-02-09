c539ff35f12724f866666adad66a1e05d3eff596
RomanRVP <spn02@ukr.net>
2023-02-09 20:34:17 +0200

    small adjustment (added '/' at the end of the log out url)

--- account/urls.py
+++ account/urls.py
@@ -6,5 +6,5 @@ from .views import RegistrationAndLogin, UserLogOut
 urlpatterns = [
     path('registration_and_login/', RegistrationAndLogin.as_view(),
          name='reg_and_log'),
-    path('user_logout', UserLogOut.as_view(), name='user_logout'),
+    path('user_logout/', UserLogOut.as_view(), name='user_logout'),
 ]
