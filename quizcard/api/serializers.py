bfda7d198d810607255aef79d713c6a6495003eb
RomanRVP <spn02@ukr.net>
2023-02-12 23:17:43 +0200

    js requests and draws the decks if the user has them (on the main page), and the serializer now reads and returns the deck id

--- quizcard/api/serializers.py
+++ quizcard/api/serializers.py
@@ -6,6 +6,7 @@ from quizcard.models import Deck
 class DeckSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=255)
     description = serializers.CharField(allow_blank=True, allow_null=True)
+    pk = serializers.ReadOnlyField()
 
     def create(self, validated_data):
         return Deck.objects.create(**validated_data)
--- quizcard/static/quizcard/js/main_page.js
+++ quizcard/static/quizcard/js/main_page.js
@@ -5,3 +5,26 @@ new_deck_btn.onclick = () => {
     document.getElementById('create-deck').style.display = 'block';
     document.getElementById('deck-list').style.display = 'none';
 }
+
+
+function getDeckList() {
+    return fetch('/api/deck/', {
+        method: 'GET',
+    }).then(response => response.json())
+        .then(data => {
+            const deckBox = document.getElementById('deck-list')
+            console.log(data)
+            for (const deck of data['deck_list']) {
+                let deckElem = document.createElement('div')
+                deckElem.className = 'current-deck'
+                deckElem.innerHTML = deck['name']
+                deckElem.id = deck['pk']
+                let deckElemDescription = document.createElement('span')
+                deckElemDescription.textContent = deck['description']
+                deckElemDescription.className = 'current-deck-description'
+                deckElem.prepend(deckElemDescription)
+                deckBox.prepend(deckElem)
+            }
+        })
+}
+getDeckList()
