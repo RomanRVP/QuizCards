3e7ca8b474fcb2ff2bf9e0f3d424e928cd2c3961
RomanRVP <spn02@ukr.net>
2023-02-14 22:10:21 +0200

    update DeckSerializer

--- quizcard/api/serializers.py
+++ quizcard/api/serializers.py
@@ -3,10 +3,7 @@ from rest_framework import serializers
 from quizcard.models import Deck
 
 
-class DeckSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=255)
-    description = serializers.CharField(allow_blank=True, allow_null=True)
-    pk = serializers.ReadOnlyField()
-
-    def create(self, validated_data):
-        return Deck.objects.create(**validated_data)
+class DeckSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Deck
+        fields = ('name', 'description', 'pk')
