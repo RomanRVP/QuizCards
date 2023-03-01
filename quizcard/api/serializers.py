7b3a432a41711ebd3af76f4a0cb9a3ec8efa636f
RomanRVP <spn02@ukr.net>
2023-03-01 23:38:07 +0200

    creating the QuizCardSerializer

--- quizcard/api/serializers.py
+++ quizcard/api/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from quizcard.models import Deck
+from quizcard.models import Deck, QuizCard
 
 
 class DeckSerializer(serializers.ModelSerializer):
     class Meta:
         model = Deck
         fields = ('name', 'description', 'pk')
+
+
+class QuizCardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = QuizCard
+        fields = ('question', 'answer', 'deck')
